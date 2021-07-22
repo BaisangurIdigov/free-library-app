6e5c34d99fc9c2d81107113cd8e65d7c1add4c0c
kerim <abu.hanifa.536942@gmail.com>
2021-07-22 11:15:48 +0300

    add controller book.id

--- client/src/components/pages/RentedBooks.jsx
+++ client/src/components/pages/RentedBooks.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import style from './style.module.css'
-import { auth } from '../../redux/features/application';
-
+import React, { useEffect } from "react";
+import style from "./style.module.css";
+import { auth } from "../../redux/features/application";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchBooks } from "../../redux/features/book";
+import { fetchUsersId } from "../../redux/features/users";
 
 export function RentedBooks(props) {
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books.items);
+  const users = useSelector((state) => state.users.items);
 
-  if(auth) {
-    return (
+
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchUsersId());
+  }, [dispatch]);
+
+
+
+  if (auth) {
+      return (
         <ul className={style.cards}>
+          {books.map((item) => {
+            return (
               <li>
                 <a href="" className={style.card}>
-                  <img src={1} className={style.card__image} alt="" />
+                  <img src={item.img} className={style.card__image} alt="" />
                   <div className={style.card__overlay}>
                     <div className={style.card__header}>
                       <svg
@@ -19,22 +38,17 @@ export function RentedBooks(props) {
                       >
                         <path />
                       </svg>
-                      <img
-                        className={style.card__thumb}
-                        src={1}
-                        alt=""
-                      />
-                      <h3 className={style.card__title}>1}</h3>
+                      <img className={style.card__thumb} src={1} alt="" />
+                      <h3 className={style.card__title}>{1}</h3>
                       <span className={style.card__status}>{1}</span>
                     </div>
-                    <p className={style.card__description}>
-                      {1}
-                    </p>
+                    <p className={style.card__description}>{1}</p>
                   </div>
                 </a>
               </li>
+            );
+          })}
         </ul>
-    );
+      );
   }
-
 }
