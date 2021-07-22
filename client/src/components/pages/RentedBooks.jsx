77805b4146fe035ae94c6f1319ef31a6191f1b57
kerim <abu.hanifa.536942@gmail.com>
2021-07-22 16:35:22 +0300

    пока ничего

--- client/src/components/pages/RentedBooks.jsx
+++ client/src/components/pages/RentedBooks.jsx
@@ -23,32 +23,10 @@ export function RentedBooks(props) {
 
 
   if (auth) {
-      return (
-        <ul className={style.cards}>
-          {books.map((item) => {
-            return (
-              <li>
-                <a href="" className={style.card}>
-                  <img src={item.img} className={style.card__image} alt="" />
-                  <div className={style.card__overlay}>
-                    <div className={style.card__header}>
-                      <svg
-                        className={style.card__arc}
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path />
-                      </svg>
-                      <img className={style.card__thumb} src={1} alt="" />
-                      <h3 className={style.card__title}>{1}</h3>
-                      <span className={style.card__status}>{1}</span>
-                    </div>
-                    <p className={style.card__description}>{1}</p>
-                  </div>
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      );
+    return (
+     <div style={{width:100, height:300, backgroundColor:'red'}}>
+
+     </div>
+    );
   }
 }
