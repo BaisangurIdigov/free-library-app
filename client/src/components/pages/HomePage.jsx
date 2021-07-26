8e235437a4828abe08c8ec7fd5bad5d0748ff7a5
kerim <abu.hanifa.536942@gmail.com>
2021-07-26 18:57:54 +0300

    final

--- client/src/components/pages/HomePage.jsx
+++ client/src/components/pages/HomePage.jsx
@@ -100,6 +100,7 @@ export default function HomePage({ values }) {
                       variant="contained"
                       color="primary"
                       disabled={addingToRend}
+                      style={{marginBottom:10, marginLeft:5}}
                     >
                       ADD
                     </Button>
