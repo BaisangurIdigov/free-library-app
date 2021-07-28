83273cb0324843314b522d152587e1c95be264c6
Baisangur <idigov-98@mail.ru>
2021-07-28 19:06:14 +0300

    - route

--- routes/users.route.js
+++ routes/users.route.js
@@ -3,7 +3,6 @@ const { userController } = require('../controllers/users.controller');
 const { Router } = require('express')
 
 const router = Router()
-router.get('/', userController.getAllUsers)
 router.get('/users',authMiddleware, userController.getUsersId)
 // router.post('/avatar', authMiddleware, userController.avatarAdd)
 router.post('/users', userController.registerUser)
